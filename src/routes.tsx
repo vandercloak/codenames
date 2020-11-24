624740c1a6e4129f4e1b43887fe275eb4dc72ec1
Nick Vander Kolk <nicholas.vanderkolk@gmail.com>
2020-11-23 21:25:57 -0600

    attempt ga again

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,27 +1,29 @@
 import React, { useEffect } from "react";
 import Game from "./pages/game/game";
-import { Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import ReactGA from "react-ga";
 import { createBrowserHistory } from "history";
+import GA from "./utils/ga";
 
 const CodenameRouter = () => {
-  const history = createBrowserHistory();
+  // const history = createBrowserHistory();
 
-  useEffect(() => {
-    ReactGA.initialize("G-HWLJKK121Z");
-    ReactGA.set({ page: window.location.pathname }); // Update the user's current page
-    ReactGA.pageview(window.location.pathname); // Record a pageview for the given page
-  }, []);
+  // useEffect(() => {
+  //   ReactGA.initialize("G-HWLJKK121Z");
+  //   ReactGA.set({ page: window.location.pathname }); // Update the user's current page
+  //   ReactGA.pageview(window.location.pathname); // Record a pageview for the given page
+  // }, []);
 
-  // Initialize google analytics page view tracking
-  history.listen((location) => {
-    console.log("here");
-    ReactGA.set({ page: location.pathname }); // Update the user's current page
-    ReactGA.pageview(location.pathname); // Record a pageview for the given page
-  });
+  // // Initialize google analytics page view tracking
+  // history.listen((location) => {
+  //   console.log("here");
+  //   ReactGA.set({ page: location.pathname }); // Update the user's current page
+  //   ReactGA.pageview(location.pathname); // Record a pageview for the given page
+  // });
 
   return (
-    <Router history={history}>
+    <Router>
+      {GA.init() && <GA.RouteTracker />}
       <Switch>
         <Route path="/game/:id">
           <Game />
